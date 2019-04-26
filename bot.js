ef975fde21aad1b6934a7c05d92e998258d9f91e
Rahul Singh <rahul.singh12221998@gmail.com>
2019-04-26 13:19:44 +0530

    webhook created

--- bot.js
+++ bot.js
@@ -21,6 +21,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
 app.listen(process.env.PORT || 4040, () => console.log(`AvBot Started`));
 
 const logger = winston.createLogger({
